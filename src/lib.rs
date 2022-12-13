b6daf01ac1c88b81d224199eddd46778700480e8
siliang <szhang@endiag.com>
2022-12-13 13:39:32 -0800

    float operation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn float_operation() {
+        assert!(0.1 + 0.2 > 0.3);
     }
 }
